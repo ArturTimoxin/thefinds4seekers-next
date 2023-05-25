729e6acf47b7f8db2806b8039c178ae1bf33fc34
macbook <macbook@macbooks-MacBook-Pro.local>
2023-05-25 17:32:25 +0300

    (fix) add env example

--- src/shared/mail-transporter.ts
+++ src/shared/mail-transporter.ts
@@ -1,20 +1,24 @@
-import nodemailer = require("nodemailer");
+import nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
- service: process.env.SMTP,
- auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
-    }
+  service: process.env.SMTP,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
 });
 
 export default transporter;
 
-export function sendMessageToEmail({ email, subject = 'thefinds4seekers.com', message }) {
-    transporter.sendMail({
-        from: process.env.SMTP_USER,
-        to: email,
-        subject, 
-        text: message,
-    });
+export function sendMessageToEmail({
+  email,
+  subject = 'thefinds4seekers.com',
+  message,
+}) {
+  transporter.sendMail({
+    from: process.env.SMTP_USER,
+    to: email,
+    subject,
+    text: message,
+  });
 }
